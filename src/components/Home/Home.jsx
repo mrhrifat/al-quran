b37e132dd99e2fb9cae0d07a829fcd391dc975ab
mrhrifat <mrhrifat383759@gmail.com>
2023-04-30 15:26:16 +0600

    build: home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Bookmark, Hero, Surah } from '../'
+
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Bookmark />
+            <Surah />
+        </>
+    )
+}
+
+export default Home
